b1cefb91b9c0e6c38edaf3ae60f72a1e8e0fe7c0
zekatsu <katsuyoso@yahoo.co.jp>
2021-08-06 21:06:27 +0900

    prototype

--- score-calculator/src/index.js
+++ score-calculator/src/index.js
@@ -4,16 +4,41 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor() {
         super();
-        this.state = {status: 0, result: '未計算'};
+        this.state = {status: [0, 0, 0, 0, 0], result: '未計算'};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-    calcScore(x) {
+    calcStatus_(x) {
         const k = [5, 8, 10, 13, 16, 18, 21, 24, 26, 28, 29, 30, 31, 33, 34, 35, 39, 41, 42, 43, 52, 55, 66, 68, 60];
         const p = Math.floor(x / 50);
         const r = x % 50;
         const cum = k.slice(0, p).reduce((acc, cur) => acc + cur, 0);
         return Math.floor(cum * 5 + (r + 1) * k[p] / 10);
+    }
+    calcStatus() {
+        return this.state.status.reduce(
+            (acc, cur) => acc + this.calcStatus_(cur), 0
+        )
+    }
+    calcUnique() {
+        const k = (this.state.rarity < 3)? 120: 170;
+        return k * this.state.uniqueLevel;
+    }
+    calcScore() {
+        const score = this.calcStatus() + this.calcUnique();
+        return score.toString();
+    }
+    statusInput(i) {
+        return (
+        <input
+            name={i.toString()}
+            type='number' min={0} max={1200}
+            onChange={(event) => {
+                let status = this.state.status;
+                status[i] = event.target.value;
+                this.setState({status: status})
+            }} />
+        );
     }
     handleChange(event) {
         this.setState({
@@ -21,9 +46,8 @@ class App extends React.Component {
         });
     }
     handleSubmit(event) {
-        const res = this.calcScore(this.state.status).toString();
         this.setState({
-            result: res,
+            result: this.calcScore(),
         });
         event.preventDefault();
     }
@@ -34,11 +58,27 @@ class App extends React.Component {
                     <form
                         onSubmit={this.handleSubmit}
                         autoComplete='off'>
-                        <input
-                            name='status'
-                            type='number' min={0} max={1200}
-                            value={this.state.value}
-                            onChange={this.handleChange} />
+                            <div className='statusInput'>
+                                <label>ステータス</label>
+                                {this.statusInput(0)}
+                                {this.statusInput(1)}
+                                {this.statusInput(2)}
+                                {this.statusInput(3)}
+                                {this.statusInput(4)}
+                            </div>
+                            <div className='uniqueInput'>
+                                <label>星</label>
+                                <input
+                                    type='number'
+                                    name='rarity' min={1} max={5}
+                                    onChange={this.handleChange} />
+                                <label>固有レベル</label>
+                                <input
+                                    type='number'
+                                    name='uniqueLevel' min={1} max={6}
+                                    onChange={this.handleChange} />
+                            </div>
+                        <input type='submit' value='calc' />
                     </form>
                 </div>
                 <div className='Result'>
